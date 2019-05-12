13db4fed86c0adb9f4ed31788d3d40bf13b6d774
Jorge Ferreiro <jorge@ferreiro.me>
2019-05-12 17:56:46 +0200

    Created AddCard view

--- App.js
+++ App.js
@@ -30,6 +30,8 @@ class CreateDeck extends PureComponent {
   }
 
   _handleSubmitForm = () => {
+    // TODO: Implement method to persist the new desk into
+    // the store
     console.log('pressed!')
     window.alert('button pressed!!!')
   }
@@ -68,14 +70,61 @@ class CreateDeck extends PureComponent {
   }
 }
 
-class DeckAddCard extends PureComponent {
+class CreateCard extends PureComponent {
+  state = {
+    answer: '',
+    question: '',
+  }
+
+  _handleChangeAnswer = (answer) => {
+    this.setState({
+      answer
+    })
+  }
+
+  _handleChangeQuestion = (question) => {
+    this.setState({
+      question
+    })
+  }
+
+  _handleSubmitForm = () => {
+    // TODO: Implement method to add card into a deck
+    console.log('pressed!')
+    window.alert('Sending card!!!')
+  }
+
   render() {
-    
+    const {answer, question} = this.state;
 
     return (
-      <View>
-        <Text>Welcome to the dashboard</Text>
-      </View>
+      <KeyboardAvoidingView
+        style={{display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column'}}
+        behavior="padding"
+      >
+        <TextInput
+          style={{padding: 30, alignSelf: 'stretch', borderColor: 'gray', borderWidth: 1, marginBottom: 30}}
+          value={question}
+          placeholder="Question"
+          onChangeText={this._handleChangeQuestion}
+        />
+
+        <TextInput
+          style={{padding: 30, alignSelf: 'stretch', borderColor: 'gray', borderWidth: 1, marginBottom: 30}}
+          value={answer}
+          placeholder="Answer"
+          onChangeText={this._handleChangeAnswer}
+        />
+
+        <TouchableOpacity
+          onPress={this._handleSubmitForm}
+          style={{maxWidth: 250, backgroundColor: '#000', padding: 20}}
+        >
+          <Text style={{color: '#fff', fontSize: '20px'}}>
+            Submit card
+          </Text>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
     )
   }
 }
@@ -99,6 +148,7 @@ export default class App extends React.Component {
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
 
+        <CreateCard />
         <CreateDeck />
       </View>
     );
